31e7c12e23d09121583664ae29898ec4717fd1c7
溜溜 <387951031@qq.com>
2023-07-16 15:27:03 +0800

    变成api

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -8,7 +8,7 @@ import { clearToken, isAuth, setToken } from './auth'
 // 创建axios实例
 const http = axios.create({
   // baseURL: 'http://toutiao.itheima.net/v1_0',
-  baseURL:'/backend'
+  baseURL:'/api'
 
 })
 
